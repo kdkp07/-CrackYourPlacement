28cc6ba1adbc9cfbe51c806ee8a84f29eee4c8ac
Kuldeep Patidar <84802372+kdkp07@users.noreply.github.com>
2023-07-24 21:25:54 +0530

    Solved FloydWarshall.java

---
+++ graphs/FloydWarshall.java
@@ -0,0 +1,38 @@
+// Problem link : https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
+class Solution
+{
+    public void shortest_distance(int[][] mat)
+    {
+        // Code here 
+        int n = mat.length;
+        for(int i = 0 ; i < n ; i++)
+        {
+                for(int j = 0; j <n ; j++)
+                {
+                    if(mat[i][j]==-1){
+                        mat[i][j] = (int)1e9;
+                    }
+                    if(i==j){
+                        mat[i][j] =0;
+                    }
+                }
+        }
+            
+        for(int k = 0 ; k < n ; k++){
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0; j < n ; j++){
+                    mat[i][j] = Math.min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+        
+        for(int i =0; i <n ; i++){
+            for(int j = 0 ; j< n;j++){
+                if(mat[i][j]==(int)1e9){
+                    mat[i][j] = -1;
+                }
+            }
+        }
+        
+    }
+}
