aae64aeaf2714fcb362328084cc75dcce1e6d305
Kuldeep Patidar <84802372+kdkp07@users.noreply.github.com>
2023-08-04 16:33:56 +0530

    Solved twoSum.java

---
+++ Arrays/twoSum.java
@@ -0,0 +1,14 @@
+// problem link : https://leetcode.com/problems/two-sum/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+       
+        for(int i = 0; i <nums.length ; i++){
+            if(hm.containsKey(target - nums[i] )){
+                return new int[]{i,hm.get(target - nums[i])};
+            }
+            hm.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
