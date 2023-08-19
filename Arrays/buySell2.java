e109d9bc315642137f0750e9b7fbabad492226bb
Kuldeep Patidar <84802372+kdkp07@users.noreply.github.com>
2023-08-19 16:33:20 +0530

    solved buySell2.java

---
+++ Arrays/buySell2.java
@@ -0,0 +1,20 @@
+// Problem link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = prices[0];
+        int sell = 0;
+        for(int i =1 ;i<prices.length;i++){
+             if(prices[i]<buy){
+                 buy = prices[i];
+             }
+             else if(prices[i]>buy){
+                 profit += prices[i] -buy;
+                 buy = prices[i];
+                 sell =0;
+    
+             }
+        }
+        return profit;
+    }
+}
